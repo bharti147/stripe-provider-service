e8308a901ed5f37e45e5a002c71c168618c53f77
Bharti <sahanibharti71@gmail.com>
2026-04-16 16:24:08 +0530

    Initial project setup

---
+++ src/main/java/com/bharti/stripe_provider_service/StripeProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bharti.stripe_provider_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeProviderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharti/stripe_provider_service/StripeProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharti.stripe_provider_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
